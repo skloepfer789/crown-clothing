fa24924af33836dea3d64bb47d7b3888691fc8a1
Steve <skloepfer789@yahoo.com>
2021-08-11 22:18:17 -0600

    Update after creating Sign-In, before starting w/Firebase

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -30,24 +32,25 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input 
+                    <FormInput 
                         name='email' 
                         type='email' 
                         value={this.state.email}
-                        onChange={this.handleChange}
+                        handleChange={this.handleChange}
+                        label='email'
                         required 
                     />
-                    <label>Email</label>
-                    <input 
+
+                    <FormInput 
                         name='password' 
                         type='password' 
                         value={this.state.password}
-                        onChange={this.handleChange}
+                        handleChange={this.handleChange}
+                        label='password'
                         required 
                     />
-                    <label>Password</label>
 
-                    <input type='submit' value='Sign Up'></input>
+                    <CustomButton type='submit'> Sign In </CustomButton>
                 </form>
             </div>
         )
