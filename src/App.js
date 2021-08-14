8c29c6fa580fc035c03982cf4ef5a2f4f8f0b10a
Steve <skloepfer789@yahoo.com>
2021-08-13 20:05:51 -0600

    finished signIn signUp

--- src/App.js
+++ src/App.js
@@ -33,9 +33,7 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          });
-
-          console.log(this.state);
+          })
         });
       }
       //if user is null, reset function (on logout)
