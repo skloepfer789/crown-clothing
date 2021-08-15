70b3275dea13fa9d1fccbef6e8d007a6d40f641a
Steve <skloepfer789@yahoo.com>
2021-08-14 23:16:14 -0600

    added checkout page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/signin-and-signup/signin-and-signup.component';
 import Header from './components/header/header.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 import {selectCurrentUser} from './redux/user/user.selector';
 
@@ -53,7 +54,15 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route 
+            exact path='/signin' 
+            render={() => 
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage/>
+              )} />
         </Switch>
       </div>
     );
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -18,6 +18,16 @@ export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems => 
         cartItems.reduce(
-        (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity, 0
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
    )
+);
+
+//total price
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+    cartItems.reduce(
+        (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity*cartItem.price, 0
+    )
 );
