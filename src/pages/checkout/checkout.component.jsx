e54df033b328475517490c7f6fa0e997d84392d0
Steve <skloepfer789@yahoo.com>
2021-08-15 20:06:55 -0600

    built clear item. Now need to edit qty

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
-import CheckoutItem from '../../components/checkout-item.component.jsx/checkout-item.component';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import './checkout.styles.scss';
 
