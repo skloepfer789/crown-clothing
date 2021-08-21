5dedff98bd660263cda94744c091fea5c8f17210
Steve <skloepfer789@yahoo.com>
2021-08-20 21:09:45 -0600

    started implementing sagas

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,31 @@
+import { takeEvery, call, put } from "redux-saga/effects";
+
+import { firestore, convertCollectionShapshotToMap } from "../../firebase/firebase.utilis";
+
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from "./shop.actions";
+
+import ShopActionTypes from "./shop.types";
+
+export function* fetchCollectionsAsync() {
+    try{
+        const collectionRef = firestore.collection('collections');
+
+        const snapshot = yield collectionRef.get();
+
+        const collectionsMap = yield call(convertCollectionShapshotToMap, snapshot);
+
+        //put is the saga equivalent to "dispatch"
+        yield put(fetchCollectionsSuccess(collectionsMap));
+
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error.message));
+    }
+}
+
+export function* fetchCollectionsStart() {
+    //yeild pauses function after each call. Then finishes after last yeild
+    //takeEvery is a non-blocking code. Code doesn't stop while the calls take effect. This way code works while the collections are fetching. You can also CANCEL functions coming out of the saga. Sagas work like async code, but technically are not, since they are encapsulated in the reducers
+    yield takeEvery(
+        ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync
+    );
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,16 @@ import { createStore, applyMiddleware } from "redux";
 //install redux persist "yarn add redux-persist" for local and session storage
 import { persistStore } from "redux-persist";
 import logger from 'redux-logger';
-import thunk from "redux-thunk";
+import createSagaMiddleware from 'redux-saga';
+
+import { fetchCollectionsStart } from "./shop/shop.sagas";
 
 import rootReducer from "./root-reducer";
 
+const sagaMiddleware = createSagaMiddleware();
+
 //set up our middleware
-const middlewares = [thunk];
+const middlewares = [sagaMiddleware];
 
 //only use middleware in dev. enviorment, not deploy
 if(process.env.NODE_ENV === 'development') {
@@ -15,6 +19,8 @@ if(process.env.NODE_ENV === 'development') {
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 export const persistor = persistStore(store);
 
